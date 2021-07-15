7c03712414ba1f45247cbb26d8e112df6602f4d0
Celso Martinez <celso.martinez@exxis-group.com>
2021-07-15 13:54:07 -0400

    Se logro establecer la conexion a la base de datos

--- App_Data/Connection.cs
+++ App_Data/Connection.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using RestSharp;
 
@@ -14,17 +15,18 @@ namespace ControlDeUnidades.Controllers
             String Url= "Https://192.168.0.5:50000/b1s/v1/Login";
             try
             {
+                
                 IRestClient Client = new RestClient(Url);
                 IRestRequest Request = new RestRequest("Auth/SignIn");
 
                 Request.Method = Method.POST;
                 Request.AddHeader("Content-Type", "application/json");
                 Request.Parameters.Clear();
-                
-                Request.AddParameter("application/json", "\"CompanyDB\": \"LOCALIZACION\",\"UserName\": \"manager5\",\"Password\": \"12345\"", ParameterType.RequestBody);
 
+                Request.AddParameter("application/json", "{\"CompanyDB\": \"LOCALIZACION\",\"UserName\": \"manager5\",\"Password\": \"12345\"}", ParameterType.RequestBody);
+                ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
                 var response = Client.Post(Request);
-                if (response.ErrorException.ToString() == "")
+                if (response.StatusCode.ToString() == "OK")
                 {
                     result = true;
                 }
