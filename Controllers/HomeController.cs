8acfc9b003863e95a7c446d9a5e5d06004a39e40
Chavy Paredes <chavy_70@hotmail.com>
2021-07-26 11:08:01 -0400

    Modificacion del front responsive

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -171,7 +171,40 @@ namespace ControlDeUnidades.Controllers
             }
         }
 
-        
+        /*
+         * Obtengo las unidades por torre seleccionada
+         */
+        [HttpGet]
+        public IActionResult obtenerInfoUnidades(string idUnidad)
+        {
+            try
+            {
+                var funciones = new Functions();
+                bool success = false;
+                // Obtengo los valores
+                var res = funciones.obtenerUnidades(idUnidad);
+                if (res.Contains("[")) success = true;
+                // Envio de valores en formato JSON
+                var json = Json(new
+                {
+                    success = success,
+                    responseText = res
+                });
+                return json;
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message;
+                // En caso de error se envia el texto por JSON
+                var json = Json(new
+                {
+                    success = false,
+                    responseText = error
+                });
+                return json;
+            }
+        }
+
         public IActionResult Privacy()
         {
             return View();
